8b4d05b590beb5c9e35a33917d3dcd461744d3bd
Ravipaliwal1434 <138785779+Ravipaliwal1434@users.noreply.github.com>
2024-01-08 16:14:32 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,81 @@
+(function($) { 
+"use strict";
+
+/*================================================================= 
+    pre loader 
+==================================================================*/
+$('.js-preloader').preloadinator({
+  animation: 'fadeOut',
+  animationDuration: 400
+});
+
+
+/* Text Animation on Hero Area */
+$('#tech-tools').textition({
+      animation: 'ease-out',
+      map: {x: 20, y: 100, z: 0},
+      autoplay: true,
+      interval: 3,
+      speed: 1
+   });
+
+
+
+
+/*================================================================= 
+    Navbar fixed top
+==================================================================*/
+$(document).ready(function () {
+
+    var menu = $('.site-header nav');
+    var origOffsetY = $('.hero-area').height();
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.site-header nav').addClass('fixed-top');
+            
+        } else {
+            $('.site-header nav').removeClass('fixed-top');
+           
+        }
+    }
+
+    document.onscroll = scroll;
+
+});
+
+
+
+/*================================================================= 
+    Animating numbers
+==================================================================*/
+$('.counter').counterUp({
+    delay: 10,
+    time: 3000
+});
+
+
+/*================================================================= 
+    Progress bar animation
+==================================================================*/
+var waypoint = new Waypoint({
+  element: document.getElementById('skill-section'),
+  handler: function() {
+    $('.progress .progress-bar').css("width",function() {
+      return $(this).attr("aria-valuenow") + "%";
+  })
+  },
+  //offset: 'bottom-in-view',
+  offset: 700,
+})
+
+
+/*================================================================= 
+    Animate on scroll initialization
+==================================================================*/
+AOS.init({
+  once: true,
+});
+
+})(jQuery);
+
